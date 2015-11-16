ab10ed78fcaae94b72a15dd1e0b23f5735d38ef8
Guy Sagnes <guy.sagnes@mni.thm.de>
2015-11-16 23:16:07 +0100

    initial version based on luky

---
+++ bb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#cd ./meta-ivi/meta-ivi/conf
+#TEMPLATECONF=`pwd`;
+#export TEMPLATECONF=`echo $TEMPLATECONF`;
+#echo "templateconf is $TEMPLATECONF";
+#cd ../../../
+
+source ./poky/oe-init-build-env ./build
+#bitbake ivi-image
